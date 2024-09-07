5ab4a325646c66352334ef4adecafc94f65a1cf5
Daniel Melchor <danielmelchor3612@gmail.com>
2024-09-07 12:28:51 -0700

    upload SQL files- project

--- Project_SQL/query_1.sql
+++ Project_SQL/query_1.sql
@@ -3,3 +3,5 @@ SELECT *
 FROM job_postings_fact
 WHERE salary_hour_avg > 700000
 LIMIT 1000
+--remember that we have to commit our changes and then we have to sync changes 
+--this is all done in the 
